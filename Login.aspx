598f41373adaddab990d084fadaba29b2d069a78
18738 <18738@MOBA-6-PLUS>
2023-07-03 11:27:42 +0800

    添加项目文件。

---
+++ Login.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>登录</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" href="image/icon.ico" />
+</head>
+<body>
+    <form runat="server" id="main">
+    <div class="box">
+        <h1 style="font-size:50px">Login</h1>
+          <div class="input-box">
+            <asp:Label runat="server" CssClass="input-box" Font-Size="X-Large">账号</asp:Label>
+            <asp:TextBox runat="server" ID="txtBox1"></asp:TextBox>
+          </div>
+          <div class="input-box">
+            <asp:Label runat="server" CssClass="input-box" Font-Size="X-Large">密码</asp:Label>
+            <asp:TextBox runat="server" TextMode="Password" ID="txtBox2"></asp:TextBox>
+          </div>
+        <div class="btn-box">
+            <div class="btn-box"> 
+                    <button id="lg_btn" type="button" class="button" runat="server" onserverclick="btn_lg_click">登录</button>
+                    <button id="reg_btn" type="button" class="button" runat="server" onserverclick="btn_reg_click">注册</button>
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_lg_click(object sender, EventArgs e)
+    {
+        string username = txtBox1.Text;
+        string password = txtBox2.Text;
+        if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        { 
+            Response.Write("未填写用户名或密码！");
+            return;
+        }
+        string conStr = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Shopping_System;Data Source=MOBA-6-PLUS";
+        OleDbConnection con = new OleDbConnection(conStr);
+        con.Open();
+        string log = "SELECT COUNT(*) FROM [user] WHERE username = ? AND password = ?";
+        OleDbCommand cmd = new OleDbCommand(log,con);
+        cmd.Parameters.AddWithValue("?", username);
+        cmd.Parameters.AddWithValue("?", password);
+        int count = (int)cmd.ExecuteScalar();
+        if(count > 0)
+        { 
+            Response.Write("登录成功！"); 
+        }
+        else
+        { 
+            Response.Write("账号或密码错误");
+        }
+        con.Close();
+    }
+    
+    protected void btn_reg_click(object sender, EventArgs e)
+    {
+        string username = txtBox1.Text;
+        string password = txtBox2.Text;
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            Response.Write("未填写用户名或密码！");
+            return;
+        }
+        string conStr = "Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Shopping_System;Data Source=MOBA-6-PLUS";
+        OleDbConnection con = new OleDbConnection(conStr);
+        con.Open();
+        string exist = "SELECT COUNT(*) FROM [user] WHERE username = ?";
+        OleDbCommand cmd = new OleDbCommand(exist, con);
+        cmd.Parameters.AddWithValue("?", username);
+        int count = (int)cmd.ExecuteScalar();
+        if (count > 0)
+        { Response.Write("用户已存在，注册失败"); }
+        else
+        {
+            string sql = "insert into [user](username,password) values ('" + txtBox1.Text + "','" + txtBox2.Text + "')";
+            OleDbCommand reg = new OleDbCommand(sql, con);
+            reg.ExecuteNonQuery();
+            Response.Write("注册成功！");
+        }
+        con.Close();
+    }
+}
